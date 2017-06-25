d4fb4159776bf9033e5fb99065ec9afc83b53451
Kristof Boucher Charbonneau <kristof.b.c@me.com>
2017-06-25 23:17:51 +0900

    Fix date

--- App/Utils/Date.ts
+++ App/Utils/Date.ts
@@ -1,6 +1,6 @@
 export function formatDate(date:Date):string {
     const day = date.getDate();
-    const month = ['Janvier', 'Février', 'Mars', 'Avril', 'May', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'][date.getMonth()-1];
+    const month = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'][date.getMonth()];
     const year = date.getFullYear();
     
     return [day, month, year].join(' ');
